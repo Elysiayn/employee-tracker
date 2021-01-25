b2bc13f540f0c9d4b0a977f274f6724346d478a6
Elysiayn <wendy.lemus.wl@gmail.com>
2021-01-24 19:51:42 -0500

    added function to get department data

--- db/schema.sql
+++ db/schema.sql
@@ -18,12 +18,13 @@ CREATE TABLE role (
     PRIMARY KEY (id)
 );
 
---table for employee info
+-- table for employee info
 CREATE TABLE employee (
     id INTEGER(10) AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(title),
     manager_id INTEGER,
     FOREIGN KEY (manager_id) REFERENCES role(id),
     PRIMARY KEY (id)
--- server.js
+++ server.js
@@ -1,25 +1,47 @@
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const {
+    connection,
+    db
+} = require('./db/database');
+const questions = require('./lib/questions');
 
-// connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'coding',
-    database: 'business_db'
-});
-
-connection.connect(err => {
-    if (err) throw err;
-    console.log('Connection Suceessful')
-    afterConnection();
-});
-
-//asks for which table to select from
-afterConnection = () => {
-    connection.query('SELECT * FROM ? WHERE ?', function (err, res) {
-        if (err) throw err;
-        console.log(res);
-        connection.end();
-    });
+// to view departments
+getDepartments = () => {
+    console.log('Veiwing all departments...\n');
+    connection.query('SELECT * FROM department'),
+        function (err, res) {
+            if (err) throw err;
+            console.log(res);
+        };
 };
+
+
+// creates new department
+// createDepartment = (name) => {
+//     console.log('Adding a new department...\n');
+//     const query = connection.query(
+//         'INSERT INTO department SET ?',
+//         function (err, res) {
+//             if (err) throw err;
+//             console.log(res.affectedRows + 'New department sucessfully added!\n');
+//         }
+//     );
+//     console.log(query.sql);
+// };
+
+
+
+// function to begin prompt
+function init() {
+    return inquirer.prompt(questions)
+        .then(res => {
+            switch (res.startPrompt) {
+                case 'View all departments':
+                    getDepartments();
+                    break;
+            };
+        });
+};
+
+init();
